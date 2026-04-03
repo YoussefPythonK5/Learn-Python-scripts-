cbd38ddf1301f4825791d28d3f1c14aab31a0394
Youssef <ya103857@gmail.com>
2026-04-03 09:58:18 +0200

    Add files via upload

---
+++  Learn how to work with text in Python .py
@@ -0,0 +1,5 @@
+name = "Youssef"
+print (  type  (name)  )
+#YOUSSEF
+names = "youssef"
+print (names.upper())
---
+++ Learn about Python scripts.py
@@ -0,0 +1,7 @@
+name = "Youssef"
+print (" my name is " +  name)
+age = "16"
+print ( " my age is " + age)
+
+
+
